b8505216d6bcdd13d4689a436147d4e78ee78e06
Ben Hekster <bhekster@bluekai.com>
2013-06-03 14:18:41 -0700

    Initial Commit

---
+++ LocalGit/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LocalGit
+//
+//  Created by Ben Hekster on 2013/06/03.
+//  Copyright (c) 2013 Ben Hekster. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
